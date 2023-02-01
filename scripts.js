dfeca0cca6af4392cd9754a261e83f2985fe6e52
Rohith Cheloor <rohithcm101@gmail.com>
2023-02-01 15:14:06 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,5 @@
+const menuClicked = () => {
+document.getElementById('ham-menu-button').classList.toggle('open');
+document.getElementById('page-links').classList.toggle('active');
+document.getElementById('social-networks').classList.toggle('active');
+}
